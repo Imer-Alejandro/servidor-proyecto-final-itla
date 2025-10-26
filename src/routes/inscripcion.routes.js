2750323146fcab28e3b55a4d3e8fc6ea85e28d7a
imer alejandro <consymer1@gmail.com>
2025-10-26 11:57:53 +0100

    implement incription course

--- src/routes/inscripcion.routes.js
+++ src/routes/inscripcion.routes.js
@@ -1,6 +1,20 @@
 import { Router } from "express";
+import {
+  obtenerInscripciones,
+  obtenerPorEstudiante,
+  obtenerPorCurso,
+  crearInscripcion,
+  actualizarInscripcion,
+  eliminarInscripcion,
+} from "../controllers/inscripcion.controller.js";
+
 const router = Router();
 
-// ...definición de endpoints
+router.get("/", obtenerInscripciones);
+router.get("/estudiante/:id", obtenerPorEstudiante);
+router.get("/curso/:id", obtenerPorCurso);
+router.post("/", crearInscripcion);
+router.put("/:id", actualizarInscripcion);
+router.delete("/:id", eliminarInscripcion);
 
 export default router;
